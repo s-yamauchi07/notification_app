1760f4b5719c43a0bc45b0afa1b2dba582429486
s-yamauchi07 <hand1nhand306@gmail.com>
2025-04-20 16:28:15 +0900

    通知設定の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+"use client"
+
 import Link from "next/link";
 import { Button } from '@mui/material';
 
 export default function Home() {
   return (
-    <div className="m-10">
+    <div className="m-10 flex flex-col gap-4">
       <Link href="users/signup">
         <Button variant="outlined">新規登録</Button>
+      </Link>
+      <Link href="users/signin">
+        <Button variant="outlined">ログイン</Button>
       </Link>
     </div>
   );
